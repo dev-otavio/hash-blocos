a881d0814c8ecd153e94e7290f9f85c477667fd1
otavio <dev-otavio@users.noreply.github.com>
2020-08-07 15:58:50 -0300

    Criada função para cópia de array unidimensional.

--- src/arrays.ts
+++ src/arrays.ts
@@ -18,3 +18,75 @@ export function criarArray(n: number): Array<number> {
 
 	return array;
 }
+
+/**
+*@param {Array<number>} original - Array a ser copiada.
+*@param {number} inicio - Índice do primeiro elemento a ser copiado.
+*@param {number|undefined} elementos - Número de elementos a serem copiados.
+*@return {Array<number>} Uma array com `elementos` elementos da original a partir de `inicio`.
+Caso `elementos` seja `undefined`, a array irá conter (original.length - inicio) elementos da `original` copiados a partir de `inicio`.
+*/
+export function copiarArray(original: Array<number>, inicio: number, elementos?: number): Array<number>;
+export function copiarArray(original: Array<number>, inicio: number, elementos: number): Array<number>;
+export function copiarArray(original: Array<number>, inicio: number, elementos: any): Array<number> {
+	if (elementos === undefined) {
+		const copia: Array<number> = [];
+		const limite = original.length;
+
+		while (inicio < limite) {
+			copia.push(original[inicio]);
+			inicio += 1;
+		}
+
+		return copia;
+	}
+
+	const copia: Array<number> = [];
+	const limite = elementos + inicio;
+
+	if (limite > original.length) {
+		throw new Error(`Argumento inválido: ${elementos}. ${original.length - inicio} elementos disponíveis a partir posição ${inicio}.`);
+	}
+
+	while (inicio < limite) {
+		copia.push(original[inicio]);
+		inicio += 1;
+	}
+
+	return copia;
+}
+
+/**
+*@param {number} linhas - Número de linhas desejadas para o array.
+*@param {number} colunas - Número de colunas desejadas para o array.
+*@return {Array<Array<number>>} Um array bidimensional de `linhas` * `colunas` elementos inicializados com valor 0.
+*/
+export function criarArray2D(linhas: number, colunas: number): Array<Array<number>> {
+	if (linhas < 0) {
+		throw new Error(`Argumento inválido: ${linhas}`);
+	}
+
+	if (colunas < 0) {
+		throw new Error(`Argumento inválido: ${colunas}`);
+	}
+
+	const array: Array<Array<number>> = [];
+
+	let ls = 0;
+
+	while (ls < linhas) {
+
+		array.push([]);
+
+		let cs = 0;
+		while (cs < colunas) {
+			array[ls][cs] = 0;
+			cs += 1;
+		}
+
+		ls += 1;
+
+	}
+
+	return array;
+}
--- tests/arrays-test.ts
+++ tests/arrays-test.ts
@@ -1,4 +1,4 @@
-import { criarArray } from "../src/arrays";
+import { criarArray, copiarArray, criarArray2D } from "../src/arrays";
 
 
 test("criarArray(n) deve criar um array de n elementos com valor inicial 0 para n >= 0.", function (){
@@ -19,4 +19,84 @@ test("criarArray(n) lança exceção para n negativo.", function () {
 
 	expect(e).not.toBeUndefined();
 	expect(e instanceof Error).toBe(true);
+});
+
+test("copiarArray(xs, 0, 0) deve retornar array vazio para qualquer array `xs`.", function () {
+	const original = criarArray(5);
+
+	expect(copiarArray(original, 0, 0).length).toBe(0);
+});
+
+const testeCopiaElementosUndefined = "copiarArray(xs, inicio) deve retornar array com elementos copiados de `xs` a partir de `inicio` até última posição de `xs`.";
+test(testeCopiaElementosUndefined, function () {
+	const original = [0, 0, -1, 0, -1];
+	const inicio = 1;
+	const copia = copiarArray(original, inicio);
+	expect(copia).toEqual(original.slice(inicio));	
+	expect(copia.length).toBe(original.length - inicio);
+});
+
+const testeCopiaComInicioElementos = "copiarArray(xs, inicio, elementos) deve retornar array com `elementos` elementos copiados de `xs` a partir de `inicio`.";
+test(testeCopiaComInicioElementos, function () {
+	const original = [0, 0, -1, 0, -1];
+	const inicio = 2;
+	const elementos = 3;
+	const copia = copiarArray(original, inicio, elementos);
+	expect(copiarArray.length).toBe(elementos);
+	expect(copia).toEqual(original.slice(inicio));
+});
+
+const testeSolicitandoElementosEmExcesso = "copiarArray(xs, inicio, elementos) lança exceção se `elementos` > `original`.length - `inicio`.";
+test(testeSolicitandoElementosEmExcesso, function() {
+	const inicio = 0;
+	const original = [0, 0, -1, 0, -1];
+	const elementos = original.length - inicio + 1;
+
+	let e: Error;
+	try {
+		copiarArray(original, inicio, elementos);
+	} catch (exception) {
+		e = exception;
+	}
+
+	expect(e).not.toBeUndefined();
+	expect(e instanceof Error).toBe(true);
+});
+
+const testeCriarArray2D = `criarArray2D(l, c) deve criar um array bidimensional de \`l\` linhas e
+\`c\` colunas representando uma matriz de \`l\` * \`c\` elementos com valor inicial 0.`;
+test(testeCriarArray2D, function() {
+	const linhas = 3;
+	const colunas = 5;
+
+	const array = criarArray2D(linhas, colunas);
+	expect(array.length).toBe(linhas);
+	expect(array.every(l => l.length === colunas)).toBe(true);
+	expect(array.every(l => l.every(e => e === 0))).toBe(true);
+});
+
+test("criarArray2D(l, c) lança exceção para `l` ou `c` negativos.", function () {
+
+	let e1: Error;
+
+	try {
+		criarArray2D(-1, 4);
+	} catch (exception) {
+		e1 = exception;
+	}
+
+	expect(e1).not.toBeUndefined();
+	expect(e1 instanceof Error).toBe(true);
+
+	let e2: Error;
+
+	try {
+		criarArray2D(4, -1);
+	} catch (exception) {
+		e2 = exception;
+	}
+
+	expect(e2).not.toBeUndefined();
+	expect(e2 instanceof Error).toBe(true);
+
 });
