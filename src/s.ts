c79d4bae3b1a0e8b1f3c0cf5d9a349e793f164e1
otavio <dev-otavio@users.noreply.github.com>
2020-08-08 18:16:25 -0300

    Criado modelo inicial para um s.

---
+++ src/s.ts
@@ -0,0 +1,12 @@
+import { Bloco } from "./bloco";
+
+
+export class S extends Bloco {
+	representacoes = [
+		[[ 0, -1, -1],
+		 [-1, -1,  0]],
+		[[-1,  0],
+		 [-1, -1],
+		 [ 0, -1]]
+	];
+}
---
+++ tests/s-test.ts
@@ -0,0 +1,28 @@
+import { S } from "../src/s";
+
+
+test("S deve possuir 2 representacões possíveis.", function () {
+	const s = new S;
+	expect(s.representacoes.length).toBe(2);
+});
+
+test("Representações de S devem ser distintas", function () {
+	const s = new S;
+	const r1 = s.representacao;
+	s.rotacionar();
+	const r2 = s.representacao;	
+
+	expect(r1).not.toStrictEqual(r2);
+});
+
+test("rotacionar deve alternar as representações de S de forma cíclica.", function () {
+	const s = new S;
+	const r1 = s.representacao;
+	s.rotacionar();
+	const r2 = s.representacao;	
+
+	s.rotacionar();
+	expect(s.representacao).toStrictEqual(r1);
+	s.rotacionar();
+	expect(s.representacao).toStrictEqual(r2);
+});
