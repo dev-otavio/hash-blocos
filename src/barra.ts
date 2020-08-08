bc00c91eb6e5e29fca2dd0073c8915bda40a733d
otavio <dev-otavio@users.noreply.github.com>
2020-08-08 17:12:13 -0300

    Criado modelo inicial para uma barra.

---
+++ src/barra.ts
@@ -0,0 +1,12 @@
+import { Bloco } from './bloco';
+
+
+export class Barra extends Bloco {
+	representacoes = [
+		[[-1, -1, -1, -1]], // horizontal
+		[[-1],
+		 [-1],
+		 [-1],
+		 [-1]], // vertical
+	];
+}
