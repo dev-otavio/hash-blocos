06bd7b9712df553a12006ff5d0817afdc55a0f15
otavio <dev-otavio@users.noreply.github.com>
2020-08-10 13:56:58 -0300

    Alterado formatação no arquivo do modelo bloco L e alterado pre-commit hook tornando executável.

--- src/l.ts
+++ src/l.ts
@@ -2,25 +2,26 @@ import { Bloco } from "./bloco";
 
 export class L extends Bloco {
     representacoes = [
+        // prettier-ignore
         [
-            [-1, 0],
-            [-1, 0],
+            [-1,  0],
+            [-1,  0],
             [-1, -1],
         ],
-
+        // prettier-ignore
         [
             [-1, -1, -1],
-            [-1, 0, 0],
+            [-1,  0,  0],
         ],
-
+        // prettier-ignore
         [
             [-1, -1],
-            [0, -1],
-            [0, -1],
+            [ 0, -1],
+            [ 0, -1],
         ],
-
+        // prettier-ignore
         [
-            [0, 0, -1],
+            [ 0,  0, -1],
             [-1, -1, -1],
         ],
     ];
